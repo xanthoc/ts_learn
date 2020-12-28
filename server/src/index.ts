bccbd51aebf7395d3617e34f99adee0fb21b1805
Cheolju Hwang <xanthoc68@gmail.com>
2020-12-28 23:45:25 +0900

    daily commit

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { router } from "./routes/loginRoutes";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["asdf"],
+  })
+);
+
+app.use(router);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
